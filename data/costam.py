24d3d4b9bc843c7ec345b0bb26279b96e7fbdaf6
kluczak12 <kluczak40@gmail.com>
2026-04-17 00:24:08 +0200

    Postawienie bazy danych i zdefiniowanie jej struktury

--- data/costam.py
+++ data/costam.py
@@ -1,16 +1,22 @@
 import cv2
 import os
+from db import init_db
  
 # test zmiennych środowiskowych z composa
 VIDEO_DIR = os.environ.get("VIDEO_DIR", "data/videos")
 FRAMES_DIR = os.environ.get("FRAMES_DIR", "data/frames")
+DB_DIR = os.environ.get("DB_DIR", "data/db")
 
-# te pliki muszą tam być!
 video_path = os.path.join(VIDEO_DIR, "test.mp4")
 output_dir = os.path.join(FRAMES_DIR, "test")
+db_path = os.path.join(DB_DIR, "db.sqlite")
  
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
+if not os.path.exists(DB_DIR):
+    os.makedirs(DB_DIR)
+
+init_db(db_path)
 
 vidcap = cv2.VideoCapture(video_path)
 frame_num = 0
@@ -21,3 +27,4 @@ while success:
     success, image = vidcap.read()
     frame_num += 1
 vidcap.release()
+
