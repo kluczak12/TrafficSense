fcf2eb7e2e7229796dc7021130c1f2b3c0b4a47e
Jakub Smyczyński <jakub@smyczek.net>
2026-04-17 16:14:36 +0200

    ekstrakcja klatek - działa, ale volume rośnie zbyt mocno

---
+++ data/costam.py
@@ -0,0 +1,51 @@
+# wymagania wstępne:
+# zbiór filmów w data/videos
+# zbiór adnotacji w data/annotations
+
+# UWAGA: kontener strasznie rośnie jak się to włącza docker compose up,
+# trzeba będzie ogarnąć co się dzieje (potencjalnie spytać KP)
+
+from os.path import join, exists
+
+if exists(".done"):
+    exit(0)
+
+import os
+import cv2
+import xml.etree.ElementTree as ET
+
+VIDEO_DIR = os.environ.get("VIDEO_DIR", "data/videos")
+ANNOTATIONS_DIR = os.environ.get("ANNOTATIONS_DIR", "data/videos/annotations")
+FRAMES_DIR = os.environ.get("FRAMES_DIR", "data/frames")
+
+videos = [f[:-4] for f in sorted(os.listdir(VIDEO_DIR)) if f.endswith('.mp4')]
+
+for vid in videos[:2]: # na razie 2 pierwsze filmy, do ostatecznego całość
+    path_to_file = join(ANNOTATIONS_DIR, vid + '.xml')
+    tree = ET.parse(path_to_file)
+    num_frames = int(tree.find("./meta/task/size").text)
+
+    video_clip_path = join(VIDEO_DIR, vid + '.mp4')
+
+    save_images_path = join(FRAMES_DIR, vid)
+    if not exists(save_images_path):
+        os.makedirs(save_images_path)
+
+    vidcap = cv2.VideoCapture(video_clip_path)
+    success, image = vidcap.read()
+    frame_num = 0
+    img_count = 0
+    if not success:
+        print(f"Failed to open the video {vid}")
+    while success:
+        img_count += 1
+        img_path = join(save_images_path, "{:05d}.jpg".format(frame_num))
+        if not exists(img_path):
+            cv2.imwrite(img_path, image, [cv2.IMWRITE_JPEG_QUALITY, 10])
+        success, image = vidcap.read()
+        frame_num += 1
+    if num_frames != img_count:
+        print(f"Did not extract all frames ({img_count} / {num_frames})")
+    print('\n')
+
+open(".done", "w").close()
