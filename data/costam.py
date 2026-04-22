d08b3e1595a438c648d2a0a6c5a29f5b9dcb292e
Klaudia Łuczak <kluczak40@gmail.com>
2026-04-22 19:18:07 +0200

    Manager bazy danych - Baza (#17)
    * Postawienie bazy danych i zdefiniowanie jej struktury
    
    * czyszczenie
    
    ---------
    
    Co-authored-by: hdudek <haniadudek05@gmail.com>

--- data/costam.py
+++ data/costam.py
@@ -1,3 +1,15 @@
+# manager bazy danych
+from db import init_db
+ 
+DB_DIR = os.environ.get("DB_DIR", "data/db")
+
+db_path = os.path.join(DB_DIR, "db.sqlite")
+ 
+if not os.path.exists(DB_DIR):
+    os.makedirs(DB_DIR)
+
+init_db(db_path)
+
 # wymagania wstępne:
 # zbiór filmów w data/videos
 # zbiór adnotacji w data/annotations
